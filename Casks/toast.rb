c5c1e7f06a2f3017bd64340a983b58d93d3cb44b
nalbam <me@nalbam.com>
2025-04-26 10:39:19 +0900

    Update homepage URL and adjust zap paths for Toast cask

--- Casks/toast.rb
+++ Casks/toast.rb
@@ -5,15 +5,15 @@ cask "toast" do
   url "https://github.com/opspresso/toast-dist/releases/download/v#{version}/Toast-#{version}-arm64.dmg"
   name "Toast"
   desc "A customizable shortcut launcher for macOS and Windows"
-  homepage "https://github.com/opspresso/toast-app"
+  homepage "https://app.toast.sh"
 
   app "Toast.app"
 
   auto_updates true
 
   zap trash: [
-    "~/Library/Application Support/toast-app",
-    "~/Library/Preferences/com.opspresso.toast-app.plist",
-    "~/Library/Saved Application State/com.opspresso.toast-app.savedState"
+    "~/Library/Application Support/Toast",
+    "~/Library/Preferences/com.opspresso.Toast.plist",
+    "~/Library/Saved Application State/com.opspresso.Toast.savedState"
   ]
 end
