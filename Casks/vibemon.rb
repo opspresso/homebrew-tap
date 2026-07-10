641117f2a59da0ac727d464d4ef1a04723adb8d5
nalbam <me@nalbam.com>
2026-07-10 18:27:20 +0900

    Add VibeMon cask version 1.13.0

---
+++ Casks/vibemon.rb
@@ -0,0 +1,19 @@
+cask "vibemon" do
+  version "1.13.0"
+  sha256 "a6063eb7cacad6243522e2364fe2c413c913f239efa04b555f9f87535a97ea13"
+
+  url "https://github.com/opspresso/vibemon-app/releases/download/v#{version}/VibeMon-#{version}-arm64.dmg"
+  name "VibeMon"
+  desc "Real-time status monitor for AI assistants with pixel art character display"
+  homepage "https://vibemon.io"
+
+  app "VibeMon.app"
+
+  auto_updates true
+
+  zap trash: [
+    "~/Library/Application Support/VibeMon",
+    "~/Library/Preferences/com.opspresso.vibemon.plist",
+    "~/Library/Saved Application State/com.opspresso.vibemon.savedState"
+  ]
+end
